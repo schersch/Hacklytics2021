7ab0d87849a3f80e22c7fb8e3f6d7b4f279c9b5e
Mark Scherschel II <schersch@gmail.com>
2021-02-05 22:50:03 -0800

    pulling function into same file to fix stupid import problem

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, jsonify, request
-from risker import get_pedictions
+
+def get_predictions(county):
+    predictions = []
+    #countyShapes = gpd.read_file("NRI_Shapefile_Counties/NRI_Shapefile_Counties.shp")
+    #countyShapes.plot()
+
+    #probably need to pre-generate all the .jpg files and just upload them
+    county_img = 'us.jpg'
+    #plt.savefig(county_img)
+    risk_level = 'TotalRisk: OMG'
+    predictions.append(county_img)
+    predictions.append(risk_level)
+    return predictions
 
 
 app = Flask(__name__)
